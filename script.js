d6b324fc0af982c4300401a7e283b046eba803e9
thanmay99 <thanmayjagadish2@gmail.com>
2024-08-17 10:09:41 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function multiply(num1,num2,num3) {
+    return num1*num2*num3;
+}
+var result=multiply(2,3,4);
+console.log(result);
+
+function divide(a,b) {
+    return a/b;
+}
+console.log(divide(10,2));
+
+function addbg(){
+    document.getElementById('bg').style.background='yellow';
+}
+   
